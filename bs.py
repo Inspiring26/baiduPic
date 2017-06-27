57b2fa4a5b9a14bd0a1b8aefb5b285145d6131de
Inspiring26 <1002990258@qq.com>
2017-06-27 09:33:44 +0800

    版本v1.6 新功能：1.提取关键字首字母2.创建首字母名的文件夹。其他：夜里测试v1.5爬取十万图片成功了！！

--- bs.py
+++ bs.py
@@ -8,6 +8,7 @@ import urllib
 import re
 from string import maketrans
 import time
+import os
 
 
 def deCode(url):
@@ -102,7 +103,8 @@ def download(word,num):
 		if len(pic_data)>10000:
 			number = number +1
 			try:
-				f=open('./py/'+str(word)+str(number)+'.jpg', 'w+')
+				#f=open('./py/'+str(word)+str(number)+'.jpg', 'w+')
+				f=open(r'./' + b + r'/' +str(word)+str(number)+'.jpg', 'w+')
 				f.write(pic_data)
 				f.close()
 			except Exception as e:
@@ -125,15 +127,99 @@ def download(word,num):
 			print '程序完成!'
 			exit(0)	
 
+
+
+
+#提取首字母模块
+def multi_get_letter(str_input): 
+  if isinstance(str_input, unicode): 
+    unicode_str = str_input 
+  else: 
+    try: 
+      unicode_str = str_input.decode('utf8') 
+    except: 
+      try: 
+        unicode_str = str_input.decode('gbk') 
+      except: 
+        print 'unknown coding' 
+        return 
+  return_list = [] 
+  for one_unicode in unicode_str: 
+    return_list.append(single_get_first(one_unicode)) 
+  return return_list 
+def single_get_first(unicode1): 
+  str1 = unicode1.encode('gbk') 
+  try:     
+    ord(str1) 
+    return str1 
+  except: 
+    asc = ord(str1[0]) * 256 + ord(str1[1]) - 65536 
+    if asc >= -20319 and asc <= -20284: 
+      return 'a' 
+    if asc >= -20283 and asc <= -19776: 
+      return 'b' 
+    if asc >= -19775 and asc <= -19219: 
+      return 'c' 
+    if asc >= -19218 and asc <= -18711: 
+      return 'd' 
+    if asc >= -18710 and asc <= -18527: 
+      return 'e' 
+    if asc >= -18526 and asc <= -18240: 
+      return 'f' 
+    if asc >= -18239 and asc <= -17923: 
+      return 'g' 
+    if asc >= -17922 and asc <= -17418: 
+      return 'h' 
+    if asc >= -17417 and asc <= -16475: 
+      return 'j' 
+    if asc >= -16474 and asc <= -16213: 
+      return 'k' 
+    if asc >= -16212 and asc <= -15641: 
+      return 'l' 
+    if asc >= -15640 and asc <= -15166: 
+      return 'm' 
+    if asc >= -15165 and asc <= -14923: 
+      return 'n' 
+    if asc >= -14922 and asc <= -14915: 
+      return 'o' 
+    if asc >= -14914 and asc <= -14631: 
+      return 'p' 
+    if asc >= -14630 and asc <= -14150: 
+      return 'q' 
+    if asc >= -14149 and asc <= -14091: 
+      return 'r' 
+    if asc >= -14090 and asc <= -13119: 
+      return 's' 
+    if asc >= -13118 and asc <= -12839: 
+      return 't' 
+    if asc >= -12838 and asc <= -12557: 
+      return 'w' 
+    if asc >= -12556 and asc <= -11848: 
+      return 'x' 
+    if asc >= -11847 and asc <= -11056: 
+      return 'y' 
+    if asc >= -11055 and asc <= -10247: 
+      return 'z' 
+    return '' 
+def translate(str_input): 
+  a = multi_get_letter(str_input) 
+  b = '' 
+  for i in a: 
+    b= b+i 
+  return b 
+
+
 if __name__ == '__main__':
-	#可以修改程序使之能够指定名字，指定爬取个数。
+#可以修改程序使之能够指定名字，指定爬取个数。
 	word = '刘亦菲古装剧照'
 	#指定个数n
-	n = 100
+	n = 10
 
 
 
-
+	b = translate(word)
+	print b
+	os.mkdir(b)
 	#更高端一点可以设置50%的失败率上限,不过有个问题，如果是数量比较小的话这个百分比就没意义了
 	l = int(n//10+2)
 	i = number = 0 
@@ -142,6 +228,7 @@ if __name__ == '__main__':
 	#记住3才是2
 	for num in xrange(1, l):
 		download(word, num)
+
 
 
 
