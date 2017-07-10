56e2baf690ed70f660bc74f97cd3086d4a81bd61
Inspiring26 <1002990258@qq.com>
2017-07-10 14:47:52 +0800

    删除调试文件

--- getpic/Count.java
+++
@@ -1,6 +0,0 @@
-public class Count{
-	// 记录成功下载的图片的数量
-	static int number = 1;
-	// 记录某个页面是否有30个图片
-	static int markNumber = 0;
-}
--- getpic/Test.java
+++
@@ -1,8 +0,0 @@
-package getpic;
-import getpic.Count;
-public class Test{
-	public static void main(String[] args) {
-		System.out.println(Count.number);
-
-	}
-}
